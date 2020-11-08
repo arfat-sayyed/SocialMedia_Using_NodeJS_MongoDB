fbba2a7bb5f340b5820f9397fec12e7b9526a1e0
Arfat Sayyed <sdarfat77@gmail.com>
2020-11-09 00:08:08 +0530

    Sign Up Added

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,7 +1,47 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req, res){
     return res.end('<h1>User Profile Is ready');
 }
 
 module.exports.posts = function(req,res){
     return res.end('<h1> User Post Is Running</h1>');
+}
+
+module.exports.signup = function(req,res){
+    return res.render('user_signup', {
+        title : "Codial Signup"
+    })
+}
+
+module.exports.signin = function(req,res){
+    return res.render('user_signin', {
+        title : "Codial SignIn"
+    })
+}
+
+module.exports.create = function(req,res){
+
+    if(req.body.password != req.body.confirm){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('Error in finding user in signing up'); return}
+
+        if(!user){
+            User.create(req.body, function(err, user){
+                if(err){console.log('Error in User Creating'); return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+
+}
+
+module.exports.createSession = function(req,res){
+    
 }
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ const users_Controllers = require('../controllers/users_controllers');
 
 router.get('/profile', users_Controllers.profile);
 router.get('/posts', users_Controllers.posts);
+router.get('/sign-up', users_Controllers.signup);
+router.get('/sign-in', users_Controllers.signin);
+
+router.post('/create', users_Controllers.create);
 
 
 
